501dce972b01f0a4c816f7f528909501afd9a072
Lee Pham <leenpham@gmail.com>
2023-09-15 22:35:27 -0700

    gen updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,40 +1,37 @@
 <script>
-    import {onMount} from 'svelte'
-    import {auth} from '../lib/firebase/firebase.client'
-    import { authStore } from '../stores/authStore';
-    import Auth from '../components/auth.svelte';
-    import {browser} from '$app/environment';
+	import { onMount } from 'svelte';
+	import { auth } from '../lib/firebase/firebase.client';
+	import { authStore } from '../stores/authStore';
+	import { browser } from '$app/environment';
 
+	onMount(() => {
+		const unsubscribe = auth.onAuthStateChanged((user) => {
+			console.log(user);
+			authStore.update((curr) => {
+				return { ...curr, isLoading: false, currentUser: user };
+			});
 
-    onMount(() => {
-        const unsubscribe = auth.onAuthStateChanged((user) => {
-            console.log(user)
-            authStore.update((curr) => {
-                return { ...curr, isLoading: false, currentUser: user};
-            });
-
-
-            if (browser && 
-                !$authStore?.currentUser &&
-                !$authStore.isLoading &&
-                window.location.pathname !== '/'
-            ) {
-                window.location.href = '/';
-            }
-        });
-        return unsubscribe
-    });
+			if (
+				browser &&
+				!$authStore?.currentUser &&
+				!$authStore.isLoading &&
+				window.location.pathname !== '/'
+			) {
+				window.location.href = '/';
+			}
+		});
+		return unsubscribe;
+	});
 </script>
 
 <main class="mainContainer">
-    <slot/>
+	<slot />
 </main>
 
 <style>
-    .mainContainer {
-        min-height: 100vh;
-		display: flex;				
-        flex-direction: column;
-    }
-
+	.mainContainer {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+	}
 </style>
